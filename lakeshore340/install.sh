6f5a8cfda8a92813c2e9cd0b73c1f306f7cef435
Paul Hockett <phockett@users.noreply.github.com>
2024-09-27 15:51:46 +0000

    add lakeshore340 support module

---
+++ lakeshore340/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=lakeshore340
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION} --org https://github.com/DiamondLightSource/
+ibek support register ${NAME}
+
+# declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
+# NONE required for StreamDevice
+# ibek support add-libs
+# ibek support add-dbds
+
+# comment out the documentation from the Makefile - idempotent because it searches
+# for lines not starting with # and inserts a # at the start of the line.
+sed -i -E 's/(^[^#].*documentation)/# \1/' ${SUPPORT}/${NAME}/Makefile
+
+# global config settings
+${FOLDER}/../_global/install.sh
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
+
+
